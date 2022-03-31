15753d4875b25bbb899605169b4ea2ebd800c445
mishra-sid <siddharthami@umass.edu>
2022-03-31 09:52:36 -0400

    Update generate exps

--- bin/generate_exps.py
+++ bin/generate_exps.py
@@ -4,9 +4,6 @@ log_dir = 'bin_out'
 slurm_path = 'slurm_logs'
 conda_path = "/home/siddharthami/anaconda3/etc/profile.d/conda.sh"
 
-def arg_to_key_val(arg):
-    return "_".join(f"{a.split(' ')[0]}={' '.join(a.split(' ')[1:]).replace(' ', '_')}" for a in arg)
-
 args = [
     ['--base_plm_family bert', '--base_plm_path bert-base-cased', '--verbalizer_init random', '--prompt_text {"soft": None, "duplicate": 1}{"placeholder":"text_a"}{"mask"}'],
     ['--base_plm_family bert', '--base_plm_path bert-base-cased', '--verbalizer_init random', '--prompt_text {"soft": None, "duplicate": 20}{"placeholder":"text_a"}{"mask"}'],
@@ -15,12 +12,11 @@ args = [
 ]
 
 if __name__ == "__main__":
-    for arg in args:
-        unique_id = arg_to_key_val(arg)
+    for uid, arg in enumerate(args):
         common = f"""#!/bin/bash
-#SBATCH --job-name=multitask-prompting-{unique_id}
-#SBATCH -o {slurm_path}/logs_{unique_id}.out
-#SBATCH -e {slurm_path}/logs_{unique_id}.err
+#SBATCH --job-name=multitask-prompting-{uid}
+#SBATCH -o {slurm_path}/logs_{uid}.out
+#SBATCH -e {slurm_path}/logs_{uid}.err
 #SBATCH --time=0-4:00:00
 #SBATCH --partition=1080ti-short
 #SBATCH --gres=gpu:1
@@ -33,7 +29,7 @@ conda activate multitask-prompting
 """
         out_path = Path(log_dir)
         out_path.mkdir(parents=True, exist_ok=True)
-        with open(out_path / f'script_{unique_id}.sh', 'w') as f:
+        with open(out_path / f'script_{uid}.sh', 'w') as f:
             f.write(common + "\n")
             f.write(f"multitask-prompting {' '.join(arg)}\n")
 
