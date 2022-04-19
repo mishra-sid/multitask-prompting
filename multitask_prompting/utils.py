2d0f114d69911d51aa54aec7fdab538b6a69fba7
asit <atarsode@umass.edu>
2022-04-19 23:57:34 +0000

    file save fix

--- multitask_prompting/utils.py
+++ multitask_prompting/utils.py
@@ -12,4 +12,5 @@ def get_num_trainable_params(args, scenarios, model):
     return sum([sum([p.numel() for p in group if p.requires_grad]) for group in param_groups])
 
 def get_uniq_str(args):
-    return f"model-{args.model}_plm_path-{args.base_plm_path}_plm-frozen-{args.tune_plm}_verbalizer_init-{args.verbalizer_init}_prompt_text-{args.prompt_text.replace(' ', '_')}"
+    s = args.prompt_text.replace(' ', '_').replace('}', '_').replace('{', '_').replace('\"', '').replace(':','').replace('.', '')
+    return f"model-{args.model}_plm_path-{args.base_plm_path}_plm-frozen-{args.tune_plm}_verbalizer_init-{args.verbalizer_init}_prompt_text-{s}"
